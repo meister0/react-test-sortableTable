8b620a68c89949fbc129ee464cb1f173383085b2
Dmitry O <dimatt2013@gmail.com>
2020-02-15 00:09:19 +0500

    Updates

---
+++ precache-manifest.c3aa19de190e59a2cc2877065772bee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ebc789ca4aae1fbe36690856d1142d",
+    "url": "/creative-test-table/index.html"
+  },
+  {
+    "revision": "2caf8244619b64f335b5",
+    "url": "/creative-test-table/static/css/main.431bcb65.chunk.css"
+  },
+  {
+    "revision": "a9e6f0da4e7c452d78e1",
+    "url": "/creative-test-table/static/js/2.be8a28b1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/creative-test-table/static/js/2.be8a28b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2caf8244619b64f335b5",
+    "url": "/creative-test-table/static/js/main.67459462.chunk.js"
+  },
+  {
+    "revision": "bcfff805019a092ec031",
+    "url": "/creative-test-table/static/js/runtime-main.45aee94f.js"
+  }
+]);
